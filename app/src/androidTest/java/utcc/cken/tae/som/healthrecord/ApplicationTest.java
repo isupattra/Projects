c4b345c937fec52afcdbf963e9af96522448ed60
supattra <supattraphramaung@gmail.com>
2015-10-08 12:26:02 +0700

    Start Project

---
+++ app/src/androidTest/java/utcc/cken/tae/som/healthrecord/ApplicationTest.java
@@ -0,0 +1,13 @@
+package utcc.cken.tae.som.healthrecord;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
