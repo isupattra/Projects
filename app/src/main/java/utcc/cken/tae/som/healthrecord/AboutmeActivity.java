5c5e281c36e85bbe6d1c98c48dcac89130352cf9
supattra <supattraphramaung@gmail.com>
2015-10-15 03:25:38 +0700

    Midterm Design  Page 4

---
+++ app/src/main/java/utcc/cken/tae/som/healthrecord/AboutmeActivity.java
@@ -0,0 +1,13 @@
+package utcc.cken.tae.som.healthrecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutmeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutme);
+    }
+}
