3345486c1b53aa287346c2f15f43be72d5aa6f43
supattra <supattraphramaung@gmail.com>
2015-10-15 01:02:50 +0700

    Midterm Design  Page 3

---
+++ app/src/main/java/utcc/cken/tae/som/healthrecord/CheckhealthActivity.java
@@ -0,0 +1,13 @@
+package utcc.cken.tae.som.healthrecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckhealthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkhealth);
+    }
+}
